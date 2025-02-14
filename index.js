ebd411a6b8164adfa1b95376b1a914d1a2514b2c
Dylan Cocoletzi <dcocoletzi49@gmail.com>
2025-02-13 20:06:04 -0800

    Added the code for the html css and js file

---
+++ index.js
@@ -0,0 +1,35 @@
+const itemName1 = document.createElement('li');
+itemName1.innerText = "Alice";
+document.getElementById('list1').append(itemName1);
+
+const itemName2 = document.createElement('li');
+itemName2.innerText = "Bob";
+document.getElementById('list1').append(itemName2);
+
+const itemName3 = document.createElement('li');
+itemName3.innerText = "Carol";
+document.getElementById('list1').append(itemName3);
+
+const itemOccupation1 = document.createElement('li');
+itemOccupation1.innerText = "Writer";
+document.getElementById('list2').append(itemOccupation1);
+
+const itemOccupation2 = document.createElement('li');
+itemOccupation2.innerText = "Teacher";
+document.getElementById('list2').append(itemOccupation2);
+
+const itemOccupation3 = document.createElement('li');
+itemOccupation3.innerText = "Programmer";
+document.getElementById('list2').append(itemOccupation3);
+
+const itemStartingPrice1 = document.createElement('li');
+itemStartingPrice1.innerText = "$30";
+document.getElementById('list3').append(itemStartingPrice1);
+
+const itemStartingPrice2 = document.createElement('li');
+itemStartingPrice2.innerText = "$50";
+document.getElementById('list3').append(itemStartingPrice2);
+
+const itemStartingPrice3 = document.createElement('li');
+itemStartingPrice3.innerText = "$70";
+document.getElementById('list3').append(itemStartingPrice3);
